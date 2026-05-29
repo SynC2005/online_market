9caf4e6214a119bd4fa86666500a7ddb62a141c6
SynC2005 <faridys74@gmail.com>
2026-05-29 21:31:44 +0700

    feat: replace script tag with next/script for improved loading strategy and nonce handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { headers } from "next/headers"; // <-- 1. IMPORT HEADERS DARI NEXT.JS
 import "./globals.css";
+import Script from "next/script";
 
 // IMPORT SPEED INSIGHTS DI SINI
 import { SpeedInsights } from "@vercel/speed-insights/next"; 
@@ -41,12 +42,12 @@ export default async function RootLayout({
         {/* LETAKKAN KOMPONENNYA DI BAWAH CHILDREN */}
         {/* SpeedInsights akan otomatis mendeteksi konfigurasi CSP Next.js */}
         <SpeedInsights />
-        <script 
-          type="text/javascript"
+        <Script 
           src="https://app.sandbox.midtrans.com/snap/snap.js"
           data-client-key={process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY}
           nonce={nonce} 
-        ></script>
+          strategy="beforeInteractive" 
+        />
         {/* 💡 CONTOH PENGGUNAAN MASA DEPAN: */}
         {/* Jika suatu saat Anda memasang Google Analytics atau script global lainnya di sini, */}
         {/* Anda WAJIB menambahkan atribut nonce seperti di bawah ini: */}
